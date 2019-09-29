d5c157d1040fd5eb58a0202ff734052d0033931f
tim ichiba <ichibat@gmail.com>
2019-09-29 10:43:53 +0900

    included bulma

--- server/app.js
+++ server/app.js
@@ -2,11 +2,18 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const helmet = require("helmet");
+const exphbs = require("express-handlebars");
 
 const app = express();
 
 //Use Helmet
 app.use(helmet());
+
+//Views directory
+app.set("views", "./server/views");
+//Express-handlebars
+app.engine("handlebars", exphbs());
+app.set("view engine", "handlebars");
 
 //Middleware
 app.use(bodyParser.json());
@@ -19,3 +26,8 @@ app.use("/api/scores", scores);
 const port = process.env.PORT || 8000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
+
+//Index route
+app.get("/", (req, res) => {
+  res.render("index");
+});
