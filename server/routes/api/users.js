004d956098fecb0d5971fdba7e123f9c435e08a5
tim ichiba <ichibat@gmail.com>
2019-10-06 14:31:21 +0900

    register form validation 28

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -12,4 +12,28 @@ router.get("/register", (req, res) => {
   res.render("users/register");
 });
 
+//Register form post
+router.post("/register", (req, res) => {
+  let errors = [];
+
+  if (req.body.password != req.body.password2) {
+    errors.push({ text: "パスワードが一致しません" });
+  }
+
+  if (req.body.password.length < 4) {
+    errors.push({ text: "パスワードは少なくとも4文字以上必要です" });
+  }
+
+  if (errors.length > 0) {
+    res.render("users/register", {
+      errors: errors,
+      email: req.body.name,
+      password: req.body.password,
+      password2: req.body.password2
+    });
+  } else {
+    res.send("passed");
+  }
+});
+
 module.exports = router;
