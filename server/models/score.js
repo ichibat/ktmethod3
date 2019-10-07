81fa0e0a93214c2aec4ee1939b220a397fbdabf4
tim ichiba <ichibat@gmail.com>
2019-10-08 07:58:49 +0900

    with Score.js model still works well because not used Score.js model

--- server/models/score.js
+++ server/models/score.js
@@ -1,4 +1,63 @@
-const express = require("express");
-const mongodb = require("mongodb");
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-const router = express.Router();
+//Create Schema
+const ScoreSchema = new Schema({
+  karteNumber: {
+    type: String,
+    required: true
+  },
+  scoreCreated: {
+    type: Date,
+    required: true
+  }
+  ktmScore.Q1Value: {
+    type: Number
+  },
+  ktmScore.Q2Value: {
+    type: Number
+  },
+  ktmScore.Q3Value: {
+    type: Number
+  },
+  ktmScore.Q4Value: {
+    type: Number
+  },
+  ktmScore.Q5Value: {
+    type: Number
+  },
+  ktmScore.Q6Value: {
+    type: Number
+  },
+  ktmScore.Q7Value: {
+    type: Number
+  },
+  ktmScore.Q8Value: {
+    type: Number
+  },
+  ktmScore.Q9Value: {
+    type: Number
+  },
+  ktmScore.Q10Value: {
+    type: Number,
+    required: true
+  },
+  ktmScore.Q11Value: {
+    type: Number,
+    required: true
+  },
+  ktmScore.Q12Value: {
+    type: Number,
+    required: true
+  },
+  ktmScore.Q13Value: {
+    type: Number,
+    required: true
+  }
+  commentUser: {
+    type: Schema.Types.ObjectId,
+    ref:"users"
+  }
+});
+
+mongoose.model("scores", ScoreSchema);
